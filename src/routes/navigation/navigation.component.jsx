874a12cd6c572f73746d5b16496f1d5e6ada7e14
EfrainAD <105945226+EfrainAD@users.noreply.github.com>
2022-12-13 20:21:39 -0500

    added a shopping cart iccon to the navbar

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,7 +1,11 @@
 import { useContext } from "react"
 import { Outlet, Link } from "react-router-dom"
+
 import { UserContext } from "../../contexts/user.context"
+
 import { signOutUser } from "../../utils/firebase/firebase.utils"
+import CartIcon from '../../components/cart-icon/cart-icon.component'
+
 import { ReactComponent as CrownLogo} from '../../assets/crown.svg'
 import './navigation.styles.scss'
 
@@ -27,6 +31,7 @@ const Navigation = () => {
             <span className='nav-link' onClick={signOutUser}>Log Out</span>
             ):(
               <Link className='nav-link' to='/auth'>Sing-In</Link>)}
+          <CartIcon />
         </div>
       </div>
       <Outlet />
