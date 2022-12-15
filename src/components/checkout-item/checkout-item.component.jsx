7373bb9a89ec7401e61f64627f4a38b49e07f56b
EfrainAD <105945226+EfrainAD@users.noreply.github.com>
2022-12-15 00:19:22 -0500

    made the style changes to match presentation

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,24 +1,37 @@
 import './checkout-item.styles.scss'
-import { useContext, useState } from 'react'
+import { useContext } from 'react'
 import { CardContext } from '../../contexts/card.context'
 
 const CheckoutItem = ({cartItem}) => {
      const {name, imageUrl, price, quantity} = cartItem
      const {addItemToCart, subtractItemFromCart, removeFromCart} = useContext(CardContext)
-     const addOneMore = () => addItemToCart(cartItem)
-     const subtractOneMore = () => subtractItemFromCart(cartItem)
-     const subtractAll = () => removeFromCart(cartItem)
+     
+     const addOneMoreHandler = () => addItemToCart(cartItem)
+     const subtractOneMoreHandler = () => subtractItemFromCart(cartItem)
+     const subtractAllHandler = () => removeFromCart(cartItem)
+
      return (
           <div className='checkout-item-container'>
-               <img src={imageUrl} alt="{name}" />
-               <div className="checkout-item-details">
-                    <span className='name'>{name}</span>
-                    <button onClick={subtractOneMore}>{'<'}</button>
-                    <span>{quantity}</span>
-                    <button onClick={addOneMore}>{'>'}</button>
-                    <span className='price'>${price * quantity}</span>
-                    <button onClick={subtractAll}>X</button>
+               <div className="image-container">
+                    <img src={imageUrl} alt="{name}" />
                </div>
+               <span className='name'>{name}</span>
+               {/* <span onClick={subtractOneMoreHandler}>{'<'}</span> */}
+               <span className='quantity'>
+                    <div className="arrow"  onClick={subtractOneMoreHandler}>
+                         &#10094;
+                    </div>
+                    <span className="value">
+                         {quantity}
+                    </span>
+                    <div className="arrow" onClick={addOneMoreHandler}>
+                         &#10095;
+                    </div>
+               </span>
+               {/* <span onClick={addOneMore}>{'>'}</span> */}
+               <span className='price'>${price * quantity}</span>
+               <div className="remove-button" onClick={subtractAllHandler}>&#10005;</div>
+               {/* <button onClick={subtractAll}>X</button> */}
           </div>
      )
 }
--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -6,12 +6,29 @@ import CheckoutItem from '../../components/checkout-item/checkout-item.component
 const Checkout = () => {
      const {cartItems, totelPrice} = useContext(CardContext)
      return (
-          <div>
+          <div className='checkout-container'>
+               <div className="checkout-header">
+                    <div className="header-block">
+                         <span>Product</span>
+                    </div>
+                    <div className="header-block">
+                         <span>Discription</span>
+                    </div>
+                    <div className="header-block">
+                         <span>Quantity</span>
+                    </div>
+                    <div className="header-block">
+                         <span>Price</span>
+                    </div>
+                    <div className="header-block">
+                         <span>Remove</span>
+                    </div>
+               </div>
                <h1>Checkout</h1>
                <div className="cart-items">
                          {cartItems.map(item => <CheckoutItem key={item.id} cartItem={item} />)}
                </div>
-               <div>TOTAL: ${totelPrice}</div>
+               <span className='total'>TOTAL: ${totelPrice}</span>
           </div>
      )
 }
