706c77c3c7c941c2c3b012a76ab4da8bff2cf66b
EfrainAD <105945226+EfrainAD@users.noreply.github.com>
2023-02-02 16:20:36 -0500

    user context now uses useReducer

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useReducer } from "react";
 import { createContext } from "react";
 import { createUserDocument, onAuthStateChangedListener } from "../utils/firebase/firebase.utils";
 
@@ -7,7 +7,36 @@ export const UserContext = createContext({
      setCurrentUser: () => null
 })
 
+const USER_ACTION_TYPES = {
+     SET_CURRENT_USER: 'SET_CURRENT_USER',
+}
+
+const userReducer = (state, action) => {
+     const { type, payload } = action
+
+     switch (type) {
+          case USER_ACTION_TYPES.SET_CURRENT_USER:
+               return {...state, currentUser: payload}
+          break
+     
+          default:
+               throw new Error(`Unhanddled action type: ${type}`)
+     }
+}
+
+const INITIAL_STATE = {currentUser: null}
+
 export const UserProvider = ({ children }) => {
+     // const [currentUser, setCurrentUser] = useState(null)
+     
+     const [{currentUser}, dispatch] = useReducer(userReducer, INITIAL_STATE)
+     const setCurrentUser = (user) => dispatch({
+          type: USER_ACTION_TYPES.SET_CURRENT_USER, 
+          payload: user
+     })
+
+     const value = {currentUser, setCurrentUser}
+
      useEffect(() => {
           const unSub = onAuthStateChangedListener((user) => {
                if (user) {
@@ -18,7 +47,5 @@ export const UserProvider = ({ children }) => {
                return user
           })
      },[])
-     const [currentUser, setCurrentUser] = useState(null)
-     const value = {currentUser, setCurrentUser}
      return <UserContext.Provider value={value}>{children}</UserContext.Provider>
 }
