4b98ec862634cde6ee86c7f474a8bcbe3a0685bf
EfrainAD <105945226+EfrainAD@users.noreply.github.com>
2023-02-04 00:08:56 -0500

    1. Set the category to be fetched than dispatch from the shop.component. 2. Set category map with the useSelector in categories-preview.component and category.component

--- src/routes/categories-preview/categories-preview.components.jsx
+++ src/routes/categories-preview/categories-preview.components.jsx
@@ -3,9 +3,12 @@ import { useContext } from "react"
 import { CategoriesContext } from "../../contexts/categories.context"
 
 import CategoryPreview from "../../components/category-preview/category-preview.component"
+import { selectCategoriesMap } from "../../store/categories/category.selector"
+import { useSelector } from "react-redux"
 
 const CategoriesPreview = () => {
-     const {categoriesMap} = useContext(CategoriesContext)
+     const categoriesMap = useSelector(selectCategoriesMap)
+     console.log('HI, CATMAP', categoriesMap)
      
      return (
           <>
--- src/routes/shop/shop.components.jsx
+++ src/routes/shop/shop.components.jsx
@@ -1,4 +1,9 @@
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import { Routes, Route } from 'react-router-dom'
+import { setCategoriesMap } from '../../store/categories/category.action'
+import { selectCategoriesMap } from '../../store/categories/category.selector'
+import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils'
 
 import CategoriesPreview from '../categories-preview/categories-preview.components'
 import Category from '../category/category.component'
@@ -6,6 +11,16 @@ import Category from '../category/category.component'
 import './shop.styles.scss'
 
 const Shop = () => {
+     const dispatch = useDispatch()
+
+     useEffect(() =>{
+          const getCategoriesMap = async () => {
+               const catogoryMap = await getCategoriesAndDocuments()
+               dispatch((setCategoriesMap(catogoryMap)))
+          }
+          getCategoriesMap()
+     },[dispatch])
+
      return (
           <Routes>
                <Route index element={<CategoriesPreview />} />
