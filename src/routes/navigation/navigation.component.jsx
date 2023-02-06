78293fcb829ec79141aadefa4da109bd56b94ab8
Efrain A. Davila <105945226+EfrainAD@users.noreply.github.com>
2023-02-06 14:41:19 -0500

    Migrate to cart redux - Select isCartOpen

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,8 +1,5 @@
-import { useContext } from "react"
 import { Outlet } from "react-router-dom"
 import { useSelector } from "react-redux"
-
-import { CartContext } from "../../contexts/cart.context"
 
 import { signOutUser } from "../../utils/firebase/firebase.utils"
 import CartIcon from '../../components/cart-icon/cart-icon.component'
@@ -11,10 +8,11 @@ import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component
 import { ReactComponent as CrownLogo} from '../../assets/crown.svg'
 import {NavigationContainer, LogoContainer, NavLinks, NavLink} from './navigation.styles'
 import { selectCurrentUser } from "../../store/user/user.selector"
+import { selectIsCartOpen } from '../../store/cart/cart.selector'
 
 const Navigation = () => {
   const currentUser = useSelector(selectCurrentUser)
-  const {isCardOpen} = useContext(CartContext)
+  const isCartOpen = useSelector(selectIsCartOpen)
   
   return (
     <>
@@ -37,7 +35,7 @@ const Navigation = () => {
               <NavLink to='/auth'>Sing-In</NavLink>)}
           <CartIcon />
         </NavLinks>
-        {isCardOpen && <CartDropdown />}
+        {isCartOpen && <CartDropdown />}
       </NavigationContainer>
       <Outlet />
     </>
