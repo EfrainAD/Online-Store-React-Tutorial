b11918031940f1c106760cdb8761dfba5bb707ae
Efrain A. Davila <105945226+EfrainAD@users.noreply.github.com>
2023-02-16 01:08:55 -0500

    added createSelector to redux category.selector

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,6 +1,17 @@
-export const selectCategories = (state) => state.categories.categories
-.reduce((acc, category) => {
+import { createSelector } from "reselect"
+
+const selectCategoryReducer = (state) => state.categories
+
+export const selectCategoriesM = createSelector(
+     [selectCategoryReducer],
+     (categoriesSlice) => categoriesSlice.categories
+)
+
+export const selectCategories = createSelector(
+     [selectCategoriesM], 
+     (categories) => categories.reduce((acc, category) => {
           const { title, items} = category
           acc[title.toLowerCase()] = items
           return acc
      }, {})
+) // ) (state) => state.categories.categories
