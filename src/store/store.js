f34fb165a9f59b0742c302c6e509e6bc3b45fbe5
Efrain A. Davila <105945226+EfrainAD@users.noreply.github.com>
2023-02-17 14:36:29 -0500

    Added persist to the redux, and optimize the middleware logger to run for dev only.

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,27 @@
 import { compose, createStore, applyMiddleware } from 'redux'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import logger from 'redux-logger'
-// import { createLogger } from 'redux-logger'
 
 import { rootReducer } from './root-reduceer'
 
-const middleWares = [logger]
-const composedEmhancers = compose(applyMiddleware(...middleWares))
+const persistConfig = {
+     key: 'root',
+     storage,
+     blacklist: ['user'],
+}
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const middleWares = 
+[
+     process.env.NODE_ENV !== 'production' && logger
+].filter(Boolean)
+
+const composeEnhancer = (process.env.NODE_ENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENTION_COMPOSE__) || compose
+
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares))
 
 // export const store = createStore(rootReducer)
-export const store = createStore(rootReducer, undefined, composedEmhancers)
+export const store = createStore(persistedReducer, undefined, composedEnhancers)
+
+export const persistor = persistStore(store)
